93158866dd5d826f2997ed8ff429f7a6c80630ea
Fernando E Jimenez <71264515+Fernando-E-Jimenez@users.noreply.github.com>
2023-05-31 08:55:04 -0300

    Add files via upload

--- task.py
+++ task.py
@@ -4,193 +4,287 @@ from openpyxl import Workbook
 from RPA.Browser.Selenium import Selenium
 import json
 
-# Define variables
-url = "https://www.nytimes.com/"
 
-# Variables for identify objects in Web. All finders are XPath
-buttonSearch="//button[@Class='css-tkwi90 e1iflr850']"
-textArea="//input[@Class='css-1j26cud']"
-buttonGo="//button[@Class='css-1gudca6 e1iflr852']"
-titlesIdXpath="//h4[@Class='css-2fgx4k']"
-datesIdXpath="//span[@Class='css-17ubb9w']"
-descriptionsIdXpath="//p[@Class='css-16nhkrn']"
-imagesIdXpath="//img[@Class='css-rq4mmj']"
-sectionButton="//button[@Class='css-4d08fs']"
-labelSections="//label[@Class='css-1a8ayg6']"
+class WebScraper:
+    """
+    Class for performing web scraping actions using Selenium.
+    """
 
-# Selenium lib
-browser_lib = Selenium()
+    def __init__(self):
+        self.browser = Selenium()
 
-# Define headers for Excel
-headers=["Title", "Date", "Description", "Picture filename", "Count Phrase", "T or F contains $"]
+    def open_browser(self, url):
+        """
+        Opens a browser and navigates to the specified URL.
+        """
+        self.browser.open_available_browser(url)
 
-# Define main functions
+    def click_search_button(self, button_search):
+        """
+        Clicks on the search button to enable the search field.
+        """
+        self.browser.click_button(button_search)
+
+    def input_text(self, text_area, text):
+        """
+        Inputs text into the specified text area.
+        """
+        self.browser.input_text(text_area, text)
+
+    def click_go_button(self, button_go):
+        """
+        Clicks on the go button to start the search.
+        """
+        self.browser.click_button(button_go)
+
+    def find_elements(self, xpath):
+        """
+        Finds web elements based on the specified XPath.
+        """
+        return self.browser.find_elements(xpath)
+
+    def wait_until_element_present(self, xpath):
+        """
+        Waits until the specified element is present on the page.
+        """
+        self.browser.wait_until_page_contains_element(xpath)
+
+    def get_text(self, element):
+        """
+        Retrieves the text of a web element.
+        """
+        return self.browser.get_text(element)
+
+    def close_browser(self):
+        """
+        Closes the browser.
+        """
+        self.browser.close_all_browsers()
+
+
+class ExcelWriter:
+    """
+    Class for writing data to an Excel file.
+    """
+
+    def __init__(self, headers):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.headers = headers
+
+    def write_headers(self):
+        """
+        Writes the headers to the Excel file.
+        """
+        for col, head in enumerate(self.headers, start=1):
+            cell = self.ws.cell(row=1, column=col)
+            cell.value = head
+
+    def write_title(self, row, title):
+        """
+        Writes the title to the specified row in the Excel file.
+        """
+        cell = self.ws.cell(row=row, column=1)
+        cell.value = title
+
+    def write_date(self, row, date):
+        """
+        Writes the date to the specified row in the Excel file.
+        """
+        cell = self.ws.cell(row=row, column=2)
+        cell.value = date
+
+    def write_description(self, row, description):
+        """
+        Writes the description to the specified row in the Excel file.
+        """
+        cell = self.ws.cell(row=row, column=3)
+        cell.value = description
+
+    def write_image_name(self, row, image_name):
+        """
+        Writes the image name to the specified row in the Excel file.
+        """
+        cell = self.ws.cell(row=row, column=4)
+        cell.value = image_name
+
+    def save_excel(self, file_path):
+        """
+        Saves the Excel file.
+        """
+        self.wb.save(file_path)
+
+
+class NewsItem:
+    """
+    Class for storing the data of a news item.
+    """
+
+    def __init__(self, title, date, description, image_name):
+        self.title = title
+        self.date = date
+        self.description = description
+        self.image_name = image_name
+
 
 def load_config_from_json(file_path):
-    # Load and config JSON for Work Items
+    """
+    Loads and returns the configuration data from a JSON file.
+    """
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
     return data
 
-def go_and_search(url, buttonSearch, textArea, search_phrase, buttonGo):
-    # Open a browser
-    browser_lib.open_available_browser(url)
 
-    # Click on the search button to enable field
-    browser_lib.click_button(buttonSearch)
-    browser_lib.input_text(textArea,search_phrase)
-    browser_lib.click_button(buttonGo)
-
-def filter_category(news_category):
-    # Apply filter if available
-    checkboxes = browser_lib.find_elements("css:.css-1qtb2wd label.css-1a8ayg6")
-
-    for checkbox in checkboxes:
-        checkbox_text = browser_lib.get_text(checkbox)
-        if news_category.lower() in checkbox_text.lower():
-            browser_lib.click(checkbox)
-            break
-
-# def filter_date(num_months):
-    # if num_months != None:
-    #     browser_lib.click_element(dateRange)
-    #     browser_lib.click_button(specificDate)
-
-def extract_elements_titles():
-    # Extract titles 
-    browser_lib.wait_until_page_contains_element(titlesIdXpath)
-    titles= browser_lib.get_webelements(titlesIdXpath)
-    return titles
-
-def extract_elements_dates():
-    # Extract dates
-    browser_lib.wait_until_page_contains_element(datesIdXpath)
-    dates= browser_lib.get_webelements(datesIdXpath)
-    return dates
-
-def extract_elements_descriptions():
-    # Extract descriptions
-    browser_lib.wait_until_page_contains_element(descriptionsIdXpath)
-    descriptions = browser_lib.get_webelements(descriptionsIdXpath)
-    return descriptions
-
-def extract_elements_images():
-    # Extract images
-    browser_lib.wait_until_page_contains_element(imagesIdXpath)
-    images = browser_lib.get_webelements(imagesIdXpath)
-    return images
-
-def open_excel(titles, dates, descriptions, images, search_phrase):
-
-    # Create Excel
-    wb = Workbook()
-
-    # Active Excel
-    ws = wb.active
-
-    # Write headers
-    for col, head in enumerate(headers, start=1):
-        cell = ws.cell(row=1, column=col)
-        cell.value= head
-
-    # Write titles
-    for row, element in enumerate(titles, start=2):
-        cell = ws.cell(row=row, column=1)
-        cell.value= element.text
-        count = count_phrase(search_phrase, element.text)
-        cell_count = ws.cell(row=row, column=5)
-        accumulate_count(cell_count, count)
-        match=find_money_formats(element.text)
-        cell_bool = ws.cell(row=row, column=6)
-        assign_boolean_value(match, cell_bool)
-
-    # Write dates    
-    for row, element in enumerate(dates, start=2):
-        cell = ws.cell(row=row, column=2)
-        cell.value= element.text
-
-    # Write descriptions
-    for row, element in enumerate(descriptions, start=2):
-        cell = ws.cell(row=row, column=3)
-        cell.value= element.text
-        count = count_phrase(search_phrase, element.text)
-        cell_count = ws.cell(row=row, column=5)
-        accumulate_count(cell_count, count)
-        match=find_money_formats(element.text)
-        cell_bool = ws.cell(row=row, column=6)
-        assign_boolean_value(match, cell_bool)
-
-    # Write images names
-    for row, element in enumerate(images, start=2):
-
-        url_image = element.get_attribute("src")
-
-        name_file = f"image_{row}.jpg"
-        path_download = f"output/{name_file}"
-        urllib.request.urlretrieve(url_image, path_download)
-
-        cell = ws.cell(row=row, column=4)
-        cell.value = name_file
-
-    # Save Excel file
-    wb.save("output/list_news.xlsx")
-
-def close_browser():
-    # Cerrar el navegador
-    browser_lib.close_all_browsers()
-
-# Define inner functions
-# It is to count the number of times the keyword appears in the texts. 
 def count_phrase(phrase, text):
+    """
+    Counts the number of times the keyword appears in the text.
+    """
     count = text.lower().count(phrase.lower())
     return count
 
-# It is to accumulate the amount already counted and the new one.
-def accumulate_count (cell, count):
 
-    current_count = cell.value
-    if current_count is None:
-        current_count = 0
-
-    new_count = current_count + count
-
-    cell.value = new_count
-
-# It is to find the correct $ format in the texts.
 def find_money_formats(text):
+    """
+    Finds the correct $ format in the text.
+    """
     pattern = r'\$[\d,]+(\.\d+)?|\d+ dollars|\d+ USD'
     matches = re.findall(pattern, text)
     return matches
 
-# It is to assign true or false if the keyword appears.
+
 def assign_boolean_value(bool_value, cell):
+    """
+    Assigns True or False based on whether the keyword appears.
+    """
     if bool_value:
         if cell.value is None or cell.value is False:
             cell.value = True
     else:
         cell.value = False
 
-# Call main functions
-def main():
 
+def go_and_search(web_scraper, url, button_search, text_area, search_phrase, button_go):
+    """
+    Performs the search on the website.
+    """
+    web_scraper.open_browser(url)
+    web_scraper.click_search_button(button_search)
+    web_scraper.input_text(text_area, search_phrase)
+    web_scraper.click_go_button(button_go)
+
+
+def filter_category(web_scraper, news_category):
+    """
+    Applies the specified category filter.
+    """
+    checkboxes = web_scraper.find_elements("css:.css-1qtb2wd label.css-1a8ayg6")
+
+    for checkbox in checkboxes:
+        checkbox_text = web_scraper.get_text(checkbox)
+        if news_category.lower() in checkbox_text.lower():
+            web_scraper.click(checkbox)
+            break
+
+
+def extract_elements_titles(web_scraper, titles_xpath):
+    """
+    Extracts the titles of the news items.
+    """
+    web_scraper.wait_until_element_present(titles_xpath)
+    titles = web_scraper.find_elements(titles_xpath)
+    return titles
+
+
+def extract_elements_dates(web_scraper, dates_xpath):
+    """
+    Extracts the dates of the news items.
+    """
+    web_scraper.wait_until_element_present(dates_xpath)
+    dates = web_scraper.find_elements(dates_xpath)
+    return dates
+
+
+def extract_elements_descriptions(web_scraper, descriptions_xpath):
+    """
+    Extracts the descriptions of the news items.
+    """
+    web_scraper.wait_until_element_present(descriptions_xpath)
+    descriptions = web_scraper.find_elements(descriptions_xpath)
+    return descriptions
+
+
+def extract_elements_images(web_scraper, images_xpath):
+    """
+    Extracts the images of the news items.
+    """
+    web_scraper.wait_until_element_present(images_xpath)
+    images = web_scraper.find_elements(images_xpath)
+    return images
+
+
+def open_excel(writer, news_items, search_phrase):
+    """
+    Writes the news item data to an Excel file.
+    """
+    writer.write_headers()
+
+    for row, news_item in enumerate(news_items, start=2):
+        writer.write_title(row, news_item.title)
+        count = count_phrase(search_phrase, news_item.title)
+        writer.write_description(row, news_item.description)
+        count += count_phrase(search_phrase, news_item.description)
+        writer.write_date(row, news_item.date)
+        writer.write_image_name(row, news_item.image_name)
+        matches = find_money_formats(news_item.title)
+        matches += find_money_formats(news_item.description)
+        assign_boolean_value(bool(matches), writer.ws.cell(row=row, column=6))
+
+        count_cell = writer.ws.cell(row=row, column=5)
+        count_cell.value = count_cell.value + count if count_cell.value else count
+
+
+def main():
     config = load_config_from_json('work_items/config.json')
 
     search_phrase = config.get('search_phrase', 'time')
     news_category = config.get('news_category', '')
-    num_months = config.get('num_months', 0)
+    num_months = config.get('num_months', 1)
+    url = config.get('url', '')
+    titles_xpath = config.get('titles_xpath', '')
+    dates_xpath = config.get('dates_xpath', '')
+    descriptions_xpath = config.get('descriptions_xpath', '')
+    images_xpath = config.get('images_xpath', '')
+    button_search = config.get('button_search', '')
+    text_area = config.get('text_area', '')
+    button_go = config.get('button_go', '')
+    file_path = config.get('file_path', '')
 
-    go_and_search(url, buttonSearch, textArea, search_phrase, buttonGo)
-    if news_category != "":
-        filter_category(news_category)
-    # if num_months is not None:
-    #     filter_date(num_months)
-    titles = extract_elements_titles()
-    dates = extract_elements_dates()
-    descriptions = extract_elements_descriptions()
-    images = extract_elements_images()
-    open_excel(titles, dates, descriptions, images, search_phrase)
-    close_browser()
+    web_scraper = WebScraper()
+    web_scraper.open_browser(url)
+    go_and_search(web_scraper, url, button_search, text_area, search_phrase, button_go)
+    filter_category(web_scraper, news_category)
 
-# Module and call main()
-if __name__ == "__main__":
+    titles = extract_elements_titles(web_scraper, titles_xpath)
+    dates = extract_elements_dates(web_scraper, dates_xpath)
+    descriptions = extract_elements_descriptions(web_scraper, descriptions_xpath)
+    images = extract_elements_images(web_scraper, images_xpath)
+
+    news_items = []
+    for title, date, description, image in zip(titles, dates, descriptions, images):
+        title_text = web_scraper.get_text(title)
+        date_text = web_scraper.get_text(date)
+        description_text = web_scraper.get_text(description)
+        image_name = image.get_attribute("src").split("/")[-1]
+        news_item = NewsItem(title_text, date_text, description_text, image_name)
+        news_items.append(news_item)
+
+    web_scraper.close_browser()
+
+    writer = ExcelWriter(['Title', 'Date', 'Description', 'Image Name', 'Count', 'Has Money Format'])
+    open_excel(writer, news_items, search_phrase)
+    writer.save_excel(file_path)
+
+
+if __name__ == '__main__':
     main()
